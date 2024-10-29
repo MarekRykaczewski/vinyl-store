feea0bc2b8963fdde5eaa384bcc7ad076717468f
Marek <m.rykaczewski99@gmail.com>
2024-10-29 19:00:13 +0100

    configure db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_NAME'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                migrations: [__dirname + '/db/migrations/*{.ts,.js}'],
+                synchronize: false,
+            }),
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
