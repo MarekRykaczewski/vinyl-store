6dadd9903176c3e3d97a8739265896887de33b55
Marek <m.rykaczewski99@gmail.com>
2024-10-31 20:16:40 +0100

    create data source

---
+++ src/db/data-source.ts
@@ -0,0 +1,27 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from 'src/user/user.entity';
+import { Review } from 'src/vinyl-records/entities/review.entity';
+import { VinylRecord } from 'src/vinyl-records/entities/vinyl-record.entity';
+import { DataSource } from 'typeorm';
+
+ConfigModule.forRoot({
+    envFilePath: [
+        `.env.${process.env.NODE_ENV}`, // Use .env.test when NODE_ENV is set to 'test'
+        '.env', // Fallback to .env
+    ],
+    isGlobal: true,
+});
+
+const configService = new ConfigService();
+
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    entities: [VinylRecord, User, Review],
+    migrations: ['src/db/migrations/*.ts'],
+    synchronize: false, // Make sure this is false for production
+});
