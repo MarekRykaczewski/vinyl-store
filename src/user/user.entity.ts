473303db6f342671ac08d1614fe461027f10706a
Marek <m.rykaczewski99@gmail.com>
2024-11-02 17:10:10 +0100

    implement admin guard

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -15,6 +15,9 @@ export class User {
   @Column({ type: 'varchar', unique: true })
       email: string;
 
+  @Column({ type: 'boolean', default: false })
+      isAdmin: boolean;
+
   @OneToMany(() => Review, (review) => review.user)
       reviews: Review[];
 }
