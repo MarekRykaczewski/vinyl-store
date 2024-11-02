6e354df77388179174a401d97c5247ccb9a5dd99
Marek <m.rykaczewski99@gmail.com>
2024-11-02 11:58:58 +0100

    add swagger to main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // Set up Swagger
+    const config = new DocumentBuilder()
+        .setTitle('Vinyl Store')
+        .setDescription('The API documentation for Vinyl Store')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
